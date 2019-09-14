5553917cd469f92ccb5785949cc781f06b4308f1
Chris Kim <oats87g@gmail.com>
2019-09-14 14:25:17 +1000

    a word

--- src/app/data/scenario.service.ts
+++ src/app/data/scenario.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParameterCodec } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpParameterCodec } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { ServerResponse } from './serverresponse';
 import { map, switchMap } from 'rxjs/operators';
