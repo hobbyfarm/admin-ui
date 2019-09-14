c7591b14705ec3a446f7cc15ba2afb8e9491df51
Chris Kim <oats87g@gmail.com>
2019-09-14 14:19:35 +1000

    forgot import

--- src/app/data/scenario.service.ts
+++ src/app/data/scenario.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParameterCodec } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { ServerResponse } from './serverresponse';
 import { map, switchMap } from 'rxjs/operators';
