002620615c90250b829d81b90cc3fcc6815bab89
Eamon Bauman <eamon@eamonbauman.com>
2019-12-21 19:57:26 -0600

    added validation

--- src/app/data/environmentavailability.ts
+++ src/app/data/environmentavailability.ts
@@ -1,7 +1,7 @@
 export class EnvironmentAvailability {
     environment: string;
     capacity_mode: string;
-    available_count: Map<string, number>;
+    available_count: {};
     available_capacity: {
         cpu: number;
         memory: number;
