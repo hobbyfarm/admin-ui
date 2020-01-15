dc52dab2e0cd5432b01805122290b2965ef18c8a
Eamon Bauman <eamon@eamonbauman.com>
2020-01-15 16:27:55 -0600

    environments list

--- src/app/configuration/environments/environments.component.ts
+++ src/app/configuration/environments/environments.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EnvironmentService } from 'src/app/data/environment.service';
+import { Environment } from 'src/app/data/environment';
 
 @Component({
   selector: 'app-environments',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./environments.component.scss']
 })
 export class EnvironmentsComponent implements OnInit {
+  public environments: Environment[] = [];
 
-  constructor() { }
+  constructor(
+    public environmentService: EnvironmentService
+  ) { }
 
   ngOnInit() {
+    this.refresh();
   }
 
+
+  public refresh() {
+    this.environmentService.list()
+    .subscribe(
+      (e: Environment[]) => this.environments = e
+    )
+  }
 }
