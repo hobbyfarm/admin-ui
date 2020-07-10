fb82e0fe156013dc1b5a4c9babdaee36e48ac848
Eamon Bauman <eamon@eamonbauman.com>
2020-07-10 17:09:40 -0500

    added finished status filtering
    Signed-off-by: Eamon Bauman <eamon@eamonbauman.com>

---
+++ src/app/event/event-status-filter/event-status-filter.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ClrDatagridFilter, ClrDatagridFilterInterface } from '@clr/angular';
+import { Subject } from 'rxjs';
+import { ScheduledEvent } from 'src/app/data/scheduledevent';
+
+@Component({
+  selector: 'event-status-filter',
+  templateUrl: './event-status-filter.component.html',
+  styleUrls: ['./event-status-filter.component.scss']
+})
+export class EventStatusFilterComponent implements OnInit, ClrDatagridFilterInterface<ScheduledEvent> {
+  enabled: boolean = true;
+
+  constructor(private filterContainer: ClrDatagridFilter) {
+    filterContainer.setFilter(this);
+  }
+
+  changes = new Subject<any>();
+
+  setActive(active: boolean): void {
+    this.enabled = active;
+    this.changes.next();
+  }
+
+  isActive(): boolean {
+    return this.enabled;
+  }
+
+  accepts(se: ScheduledEvent): boolean {
+    return !se.active;
+  }
+
+  ngOnInit(): void {
+  }
+}
