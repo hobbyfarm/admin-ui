e4d7fc6ecaed4bce59ed3d84c32643b4fd72711d
Raphael von der Grün <raphael.vondergruen@sva.de>
2022-03-23 15:07:04 +0100

    Update Angular to 12
    - also update Clarity and ngx-markdown to v12
    - align jasmine version w/ newly generated v12 project
    - regenerate package-lock.json
    
    Commands used
        npx @angular/cli@11 update @angular/core@11 @angular/cli@11 ngx-markdown@11
        npx ng update @clr/angular@5 @clr/icons@5
        npx @angular/cli@12 update @angular/core@12 @angular/cli@12
        npx ng update @clr/angular@12 @clr/icons@12
    
    After the update, the Angular build process emits a few new warnings:
      - A warning about deep imports (caused by the clarity styles).
        For a workaround see: angular/angular#35615 (comment)
    
      - Warnings about Clarity's usage of the now deprecated SASS division
        operator. For more information see: vmware/clarity#6331

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -81,7 +81,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
