17271e7a4da2403837c83121d4f534ba8b51a963
Jan-Gerrit Goebel <Jan-Gerrit.Goebel@sva.de>
2023-02-13 17:33:38 +0100

    Add subpath to services

--- src/app/data/vm-template-service-configuration.ts
+++ src/app/data/vm-template-service-configuration.ts
@@ -1,34 +1,42 @@
-import YAML from 'yaml'
+import YAML from 'yaml';
 export class VMTemplateServiceConfiguration {
-    public name: string = ""
-    public hasWebinterface = false
-    public port?: number = -1
-    public hasOwnTab?: boolean = false
-    public cloudConfigMap?: Map<string, Object> = new Map()
-    public cloudConfigString?: string = ''   
+  public name: string = '';
+  public hasWebinterface = false;
+  public port?: number = -1;
+  public path?: string = '/';
+  public hasOwnTab?: boolean = false;
+  public cloudConfigMap?: Map<string, Object> = new Map();
+  public cloudConfigString?: string = '';
 }
 
-export function getCloudConfigString(vmService: VMTemplateServiceConfiguration) {
-    return YAML.stringify(vmService.cloudConfigMap)
+export function getCloudConfigString(
+  vmService: VMTemplateServiceConfiguration
+) {
+  return YAML.stringify(vmService.cloudConfigMap);
 }
-
 
 export function vmServiceToJSON(vmService: VMTemplateServiceConfiguration) {
-   
-        let result = "{\"name\": \""+vmService.name+"\" ,\"hasWebinterface\": "+vmService.hasWebinterface
-        if (vmService.port) {
-            result += ", \"port\": "+vmService.port
-        }
-        if (vmService.hasOwnTab) {
-            result += ", \"hasOwnTab\": "+vmService.hasOwnTab
-        }
-        if (vmService.cloudConfigMap) {
-            let cloudConfigMapString = ""
-            vmService.cloudConfigMap.forEach((value, key) => {
-                cloudConfigMapString += "\""+key+"\": "+JSON.stringify(value)+" "
-            })
-            result += ", \"cloudConfigMap\": {"+cloudConfigMapString+"}"
-        }
-        result += "}"
-        return result   
+  let result =
+    '{"name": "' +
+    vmService.name +
+    '" ,"hasWebinterface": ' +
+    vmService.hasWebinterface;
+  if (vmService.port) {
+    result += ', "port": ' + vmService.port;
+  }
+  if (vmService.path) {
+    result += ', "path": ' + JSON.stringify(vmService.path);
+  }
+  if (vmService.hasOwnTab) {
+    result += ', "hasOwnTab": ' + vmService.hasOwnTab;
+  }
+  if (vmService.cloudConfigMap) {
+    let cloudConfigMapString = '';
+    vmService.cloudConfigMap.forEach((value, key) => {
+      cloudConfigMapString += '"' + key + '": ' + JSON.stringify(value) + ' ';
+    });
+    result += ', "cloudConfigMap": {' + cloudConfigMapString + '}';
+  }
+  result += '}';
+  return result;
 }
