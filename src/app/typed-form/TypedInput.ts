1da69e842a87a3cc91425033f81e2de156e467a2
Jan-Gerrit Göbel <jgoebel@uni-bremen.de>
2023-04-05 21:41:35 +0200

    Comments

--- src/app/typed-form/TypedInput.ts
+++ src/app/typed-form/TypedInput.ts
@@ -2,14 +2,15 @@ export enum TypedInputTypes{
   STRING,
   NUMBER,
   BOOLEAN,
-  ENUM
+  ENUM,
+  // COLOR or other possible custom input types
 }
 
 export class TypedInput {
-  id: string;
-  name: string;
-  group: string;
+  id: string; // id as of the metadata.name
+  name: string; // Display name of the input
+  group: string; // Group e.g. General, Provisioning etc.
   type: TypedInputTypes;
   value: any;
-  enumValues?: string[];
+  enumValues?: string[]; // If this is of type ENUM this list provides allowed values
 }
