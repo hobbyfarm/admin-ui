e1a3ce6262f3990e4e58ad90100449614bf25e78
Abdellah Brahimi <abdellah.brahimi@sva.de>
2023-05-08 15:05:21 +0200

    Content Management #312
    The Content Management when it's empty should navigate to the Either Scenarios or Course if they are available

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,7 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { RbacService } from '../data/rbac.service';
+import { ScenarioService } from '../data/scenario.service';
+import { CourseService } from '../data/course.service';
+import { Scenario } from '../data/scenario';
+import { Course } from '../data/course';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
 })
-export class ContentComponent {}
+export class ContentComponent implements OnInit, OnChanges{
+  scenarios: Scenario[] = [];
+  courses: Course[] = [];
+  selectRbac: boolean = false;
+
+  constructor(
+    public scenarioService: ScenarioService,
+    public courseService: CourseService,
+    public rbacService: RbacService,
+    private route: Router,
+    private activateRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit():void {
+    this.contentNavigation()
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.contentNavigation()
+  }
+  contentNavigation() {
+    this.rbacService.Grants('scenarios', 'get').then((allowed: boolean) => {
+      this.selectRbac = allowed;
+    });
+
+    this.rbacService.Grants('courses', 'get').then((allowed: boolean) => {
+      this.selectRbac = allowed;
+    });
+
+    this.scenarioService.list().subscribe((s: Scenario[]) => {
+      this.scenarios = s;
+      if (this.scenarios.length > 0) {
+        this.route.navigate(['scenarios'], { relativeTo: this.activateRoute });
+      } else {
+        this.courseService.list().subscribe((c: Course[]) => {
+          this.courses = c;
+          if (this.courses.length > 0)
+            this.route.navigate(['courses'], { relativeTo: this.activateRoute });
+        });
+      }
+    });
+
+
+  }
+
+}
