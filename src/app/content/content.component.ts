00b20a226cc206e89a2c3ca50cfb5d04bd3237ac
Abdellah Brahimi <119412567+abrahimi93@users.noreply.github.com>
2023-05-26 12:42:41 +0200

    Content Management #312 (#169)
    * Content Management #312
    
    The Content Management when it's empty should navigate to the Either Scenarios or Course if they are available
    
    * Get off dead code OnChange
    
    * Redirect to Scenario when there is no scenario and no courses
    
    * Refactoring the code get ride of duplication
    
    * Refactoring the code

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,7 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { RbacService } from '../data/rbac.service';
+import { ScenarioService } from '../data/scenario.service';
+import { CourseService } from '../data/course.service';
+import { Scenario } from '../data/scenario';
+import { Course } from '../data/course';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
 })
-export class ContentComponent {}
+export class ContentComponent implements OnInit {
+  constructor(
+    public rbacService: RbacService,
+    private route: Router,
+    private activateRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.contentNavigation();
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.contentNavigation();
+  }
+
+  navigateScenarios() {
+    this.route.navigate(['scenarios'], { relativeTo: this.activateRoute });
+  }
+  navigateCourses() {
+    this.route.navigate(['courses'], {
+      relativeTo: this.activateRoute,
+    });
+  }
+  contentNavigation() {
+    this.rbacService
+      .Grants('scenarios', 'list')
+      .then((scenarioAllowed: boolean) => {
+        if (scenarioAllowed) {
+          this.navigateScenarios();
+        } else {
+          this.rbacService
+            .Grants('courses', 'list')
+            .then((courseAllowed: boolean) => {
+              if (courseAllowed) {
+                this.navigateCourses();
+              }
+            });
+        }
+      });
+  }
+}
