64c65fb69d18a6dd2c57d94e0ff6f7181a9d9f2a
Philip Prinz <Philip.Prinz@sva.de>
2023-07-11 10:37:21 +0200

    Add comment to provide Reasons for custom encoder

--- src/app/data/vmtemplate.service.ts
+++ src/app/data/vmtemplate.service.ts
@@ -8,68 +8,70 @@ import { atou } from '../unicode';
 import { VMTemplate } from './vmtemplate';
 import { HttpParameterCodec } from '@angular/common/http';
 
+// Custom Encoder to prevent Angulars default En-/Decoder from decoding special Characters again after encoding them, see https://github.com/angular/angular/blob/875851776c2f1a688554e91b8f5352984a71d16b/packages/common/http/src/params.ts#L94.
+// This behaviour lead to the Data not being saved in Gargantua if it contains ';'.
 export class CustomHttpParamEncoder implements HttpParameterCodec {
-    encodeKey(key: string): string {
-        return encodeURIComponent(key);
-    }
-    encodeValue(value: string): string {
-            return encodeURIComponent(value);
-    }
-    decodeKey(key: string): string {
-            return decodeURIComponent(key);
-    }
-    decodeValue(value: string): string {
-            return decodeURIComponent(value);
-    }
+  encodeKey(key: string): string {
+    return encodeURIComponent(key);
+  }
+  encodeValue(value: string): string {
+    return encodeURIComponent(value);
+  }
+  decodeKey(key: string): string {
+    return decodeURIComponent(key);
+  }
+  decodeValue(value: string): string {
+    return decodeURIComponent(value);
+  }
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class VmtemplateService {
-
-  constructor(
-    public http: HttpClient
-  ) { }
+  constructor(public http: HttpClient) {}
 
   public list() {
-    return this.http.get(environment.server + '/a/vmtemplate/list')
-    .pipe(
+    return this.http.get(environment.server + '/a/vmtemplate/list').pipe(
       switchMap((s: ServerResponse) => {
-        return of(JSON.parse(atou(s.content)))
+        return of(JSON.parse(atou(s.content)));
       })
-    )
+    );
   }
 
   public update(template: VMTemplate) {
-    let params = new HttpParams({encoder: new CustomHttpParamEncoder()})
-    .set('id', template.id)
-    .set('name', template.name)
-    .set('image', template.image)
-    .set('config_map', JSON.stringify(template.config_map))
-   
-    return this.http.put(environment.server + "/a/vmtemplate/" + template.id + "/update", params)
+    let params = new HttpParams({ encoder: new CustomHttpParamEncoder() })
+      .set('id', template.id)
+      .set('name', template.name)
+      .set('image', template.image)
+      .set('config_map', JSON.stringify(template.config_map));
+
+    return this.http.put(
+      environment.server + '/a/vmtemplate/' + template.id + '/update',
+      params
+    );
   }
 
   public create(template: VMTemplate) {
-    let params = new HttpParams()
-    .set('name', template.name)
-    .set('image', template.image)
-    .set('config_map', JSON.stringify(template.config_map))
+    let params = new HttpParams({ encoder: new CustomHttpParamEncoder() })
+      .set('name', template.name)
+      .set('image', template.image)
+      .set('config_map', JSON.stringify(template.config_map));
 
-    return this.http.post(environment.server + "/a/vmtemplate/create", params)
+    return this.http.post(environment.server + '/a/vmtemplate/create', params);
   }
 
   public get(id: string) {
-    return this.http.get(environment.server + "/a/vmtemplate/" + id)
-    .pipe(
+    return this.http.get(environment.server + '/a/vmtemplate/' + id).pipe(
       switchMap((s: ServerResponse) => {
-        return of(JSON.parse(atou(s.content)))
+        return of(JSON.parse(atou(s.content)));
       })
-    )
+    );
   }
 
   public delete(id: string) {
-    return this.http.delete(environment.server + "/a/vmtemplate/" + id + "/delete")
+    return this.http.delete(
+      environment.server + '/a/vmtemplate/' + id + '/delete'
+    );
   }
 }
