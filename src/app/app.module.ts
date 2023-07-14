7097711221a8d89cab08b5c153c9904df2059147
PhPrinz <48761034+PhPrinz@users.noreply.github.com>
2023-07-14 11:51:41 +0200

    Otac management (#175)
    * Add Modal to manage OTACs for Scheduled Events
    
    * add filter and sorting to otac management
    
    * protect access to otacs management via rbac
    
    * protect otac generate/delete via rbac
    
    ---------
    
    Co-authored-by: Philip Prinz <Philip.Prinz@sva.de>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -94,6 +94,7 @@ import { SettingsComponent } from './configuration/settings/settings.component';
 import { TypedInputComponent } from './typed-form/typed-input.component';
 import { TypedInputFieldComponent } from './typed-form/typed-input-field.component';
 import { TypedSettingsService } from './data/typedSettings.service';
+import { OTACManagementComponent } from './event/otacmanagement/otacmanagement.component';
 
 const appInitializerFn = (appConfig: AppConfigService) => {
   return () => {
@@ -173,6 +174,7 @@ export function jwtOptionsFactory() {
     TypedInputComponent,
     TypedInputFieldComponent,
     SettingsComponent,
+    OTACManagementComponent,
   ],
   imports: [
     BrowserModule,
