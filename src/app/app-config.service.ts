ce5653b3151685d4067e2c403c31906c342d2ab9
philipab <philip231001@googlemail.com>
2023-09-01 14:30:45 +0200

    replace deprecated "toPromise()" function

--- src/app/app-config.service.ts
+++ src/app/app-config.service.ts
@@ -1,25 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppConfigService {
   private appConfig;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   loadAppConfig() {
-    return this.http.get('/config.json')
-      .toPromise() // deprecated since rsjx version 7 => replace with lastValueFrom when upgrading
-      .then(data => {
-        this.appConfig = data;
-      });
+    return lastValueFrom(this.http.get('/config.json')).then((data) => {
+      this.appConfig = data;
+    });
   }
 
   getLogo(logoPath: string) {
     const headers = new HttpHeaders();
-    headers.set('Accept', '*')
-    return this.http.get(logoPath, {headers, responseType: 'text'})
-    .toPromise() // deprecated since rsjx version 7 => replace with lastValueFrom when upgrading
+    headers.set('Accept', '*');
+    return lastValueFrom(
+      this.http.get(logoPath, { headers, responseType: 'text' })
+    );
   }
 
   getConfig() {
--- src/app/data/rbac.service.ts
+++ src/app/data/rbac.service.ts
@@ -1,53 +1,58 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { atou } from '../unicode';
 import { AccessSet } from './accessset';
-import { ApiGroup, hobbyfarmApiGroup, rbacApiGroup, Resource, Verb } from './rbac';
+import {
+  ApiGroup,
+  hobbyfarmApiGroup,
+  rbacApiGroup,
+  Resource,
+  Verb,
+} from './rbac';
 import { ServerResponse } from './serverresponse';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RbacService {
-  private static all: string = "*"
+  private static all: string = '*';
 
   private userAccess: Promise<AccessSet>;
 
-  constructor(
-    public http: HttpClient
-  ) {
+  constructor(public http: HttpClient) {
     // only load an access set if the hobbyfarm token is in place
     // otherwise we would be loading an empty access set
-    if (localStorage.getItem("hobbyfarm_admin_token")?.length > 0) {
-      this.LoadAccessSet(); 
+    if (localStorage.getItem('hobbyfarm_admin_token')?.length > 0) {
+      this.LoadAccessSet();
     } else {
-      this.userAccess = new Promise<AccessSet>(resolve => {
+      this.userAccess = new Promise<AccessSet>((resolve) => {
         resolve(null);
-      })
+      });
     }
   }
 
   // async because we want to ensure that callers of this method
-  // can wait for its completion. This is especially important in 
+  // can wait for its completion. This is especially important in
   // login.component, because we don't want to navigate the user to
   // the home page before we have completed setting up rbac.
   public LoadAccessSet(): Promise<AccessSet> {
-    this.userAccess = this.http.get(environment.server + "/auth/access")
-    .pipe(
-      map((s: ServerResponse) => {
-        const accessSet: AccessSet = JSON.parse(atou(s.content));
-        return accessSet;
-      })
-    ).toPromise(); // deprecated since rsjx version 7 => replace with lastValueFrom when upgrading
+    this.userAccess = lastValueFrom(
+      this.http.get(environment.server + '/auth/access').pipe(
+        map((s: ServerResponse) => {
+          const accessSet: AccessSet = JSON.parse(atou(s.content));
+          return accessSet;
+        })
+      )
+    );
     return this.userAccess;
   }
 
   public async Grants(resource: Resource, verb: Verb): Promise<boolean> {
-    let apiGroup : ApiGroup;
-    if (resource == "roles" || resource == "rolebindings") {
+    let apiGroup: ApiGroup;
+    if (resource == 'roles' || resource == 'rolebindings') {
       apiGroup = rbacApiGroup;
     } else {
       apiGroup = hobbyfarmApiGroup;
@@ -55,24 +60,24 @@ export class RbacService {
 
     let allowed = false;
     return this.userAccess.then((accessSet: AccessSet) => {
-      if(accessSet) {
+      if (accessSet) {
         [RbacService.all, apiGroup].forEach((a) => {
           [RbacService.all, resource].forEach((r) => {
             [RbacService.all, verb].forEach((v) => {
-              const key = "/" + a + "/" + r + "/" + v
+              const key = '/' + a + '/' + r + '/' + v;
               // this.userAccess.pipe(filter(object => object != null)).subscribe(object => {
               //   if(object && object.access[key]) {
               //     allowed = true;
               //   }
               // })
-              if(accessSet.access[key]) {
+              if (accessSet.access[key]) {
                 allowed = true;
               }
-            })
-          })
-        })
+            });
+          });
+        });
       }
       return allowed;
-    })
+    });
   }
 }
