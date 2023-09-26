2ad7982c7a22b68afbb19bdf0a350cf695752cd3
philipab <philip231001@googlemail.com>
2023-09-26 18:10:42 +0200

    Update Angular v15 to v16

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate } from '@angular/router';
+import { Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private router: Router, private helper: JwtHelperService) {}
 
   canActivate() {
