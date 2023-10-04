f3c7be5ed6e2af2b47a4650738b6a25d52d944a5
Jan-Gerrit Goebel <Jan-Gerrit.Goebel@sva.de>
2023-10-04 09:13:55 +0200

    Fix user filter by only comparing lowercase values

--- src/app/user-accescode-filter.ts
+++ src/app/user-accescode-filter.ts
@@ -4,7 +4,7 @@ import { User } from './data/user';
 export class UserAccesscodeFilter implements ClrDatagridStringFilterInterface<User>{
     accepts(item: User, search: string) {
         let resultingUserList = item.access_codes.some(ac => {
-            return ac.search(search) != -1               
+            return ac.toLowerCase().search(search.toLowerCase()) != -1               
         })
         return resultingUserList
     }
