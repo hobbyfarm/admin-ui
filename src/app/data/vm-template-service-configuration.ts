22ca8664d1aa4761d83d841362e3900876e9967e
Philip Aguilar Bremer <Philip231001@googlemail.com>
2024-09-20 08:23:39 +0200

    Add new proxy configuration options (#223)

--- src/app/data/vm-template-service-configuration.ts
+++ src/app/data/vm-template-service-configuration.ts
@@ -1,16 +1,19 @@
 import YAML from 'yaml';
-import * as uuid from 'uuid'
+import * as uuid from 'uuid';
+import { Protocol } from './protocol';
 export class VMTemplateServiceConfiguration {
   public id: string;
   public name: string;
   public hasWebinterface: boolean;
   public port?: number;
   public path?: string;
+  public protocol?: Protocol;
   public hasOwnTab?: boolean;
   public noRewriteRootPath?: boolean;
   public rewriteHostHeader?: boolean;
   public rewriteOriginHeader?: boolean;
   public disallowIFrame?: boolean;
+  public disableAuthorizationHeader: boolean;
   public cloudConfigMap?: Map<string, Object>;
   public cloudConfigString?: string;
 
@@ -19,23 +22,27 @@ export class VMTemplateServiceConfiguration {
     hasWebinterface = false,
     port: number = -1,
     path: string = '/',
+    protocol: Protocol = 'http',
     hasOwnTab: boolean = false,
     noRewriteRootPath: boolean = false,
     rewriteHostHeader: boolean = true,
     rewriteOriginHeader: boolean = false,
     disallowIFrame: boolean = false,
+    disableAuthorizationHeader: boolean = false,
     cloudConfigString: string = ''
   ) {
-    this.id = uuid.v4()
+    this.id = uuid.v4();
     this.name = name;
     this.hasWebinterface = hasWebinterface;
     this.port = port;
     this.path = path;
+    this.protocol = protocol;
     this.hasOwnTab = hasOwnTab;
     this.noRewriteRootPath = noRewriteRootPath;
     this.rewriteHostHeader = rewriteHostHeader;
     this.rewriteOriginHeader = rewriteOriginHeader;
     this.disallowIFrame = disallowIFrame;
+    this.disableAuthorizationHeader = disableAuthorizationHeader;
     this.cloudConfigString = cloudConfigString;
     this.cloudConfigMap = new Map();
     if (this.cloudConfigString.length > 0) {
@@ -59,7 +66,9 @@ export function getCloudConfigString(
 
 export function vmServiceToJSON(vmService: VMTemplateServiceConfiguration) {
   let result =
-    '{"id" : "' + vmService.id + '" , "name": "' +
+    '{"id" : "' +
+    vmService.id +
+    '" , "name": "' +
     vmService.name +
     '" ,"hasWebinterface": ' +
     vmService.hasWebinterface;
@@ -71,6 +80,9 @@ export function vmServiceToJSON(vmService: VMTemplateServiceConfiguration) {
     }
     if (vmService.path) {
       result += ', "path": ' + JSON.stringify(vmService.path);
+    }
+    if (vmService.protocol) {
+      result += ', "protocol": ' + JSON.stringify(vmService.protocol);
     }
     if (vmService.hasOwnTab) {
       result += ', "hasOwnTab": ' + vmService.hasOwnTab;
@@ -84,31 +96,42 @@ export function vmServiceToJSON(vmService: VMTemplateServiceConfiguration) {
     if (vmService.disallowIFrame) {
       result += ', "disallowIFrame": ' + vmService.disallowIFrame;
     }
+    if (vmService.disableAuthorizationHeader) {
+      result +=
+        ', "disableAuthorizationHeader": ' +
+        vmService.disableAuthorizationHeader;
+    }
     if (vmService.noRewriteRootPath) {
       result += ', "noRewriteRootPath": ' + vmService.noRewriteRootPath;
     }
   }
-  if (vmService.cloudConfigMap) {   
-    result += ', "cloudConfigMap": {' + mapToJson(vmService.cloudConfigMap, '') + '}'; //Potential alternative if es2019 or higher (configurable in tsconfig, field: lib): JSON.stringify(Object.fromEntries(vmService.cloudConfigMap))
+  if (vmService.cloudConfigMap) {
+    result +=
+      ', "cloudConfigMap": {' + mapToJson(vmService.cloudConfigMap, '') + '}'; //Potential alternative if es2019 or higher (configurable in tsconfig, field: lib): JSON.stringify(Object.fromEntries(vmService.cloudConfigMap))
   }
   result += '}';
   return result;
 }
 
 export function mapToJson(map: Map<string, any>, jsonString: string): string {
-  let isfirstElement = true
+  let isfirstElement = true;
   map.forEach((value, key) => {
-    if(!isfirstElement) {
-      jsonString += ','
+    if (!isfirstElement) {
+      jsonString += ',';
     }
-    if(typeof value === 'string' || Array.isArray(value)) {
+    if (typeof value === 'string' || Array.isArray(value)) {
       jsonString += '"' + key + '": ' + JSON.stringify(value) + ' ';
-    } else if (value instanceof Map){
+    } else if (value instanceof Map) {
       jsonString += '"' + key + '": {' + mapToJson(value, '') + '} ';
     } else {
-      jsonString += '"' + key + '": {' + mapToJson(new Map(Object.entries(value)), '') + '} ';
+      jsonString +=
+        '"' +
+        key +
+        '": {' +
+        mapToJson(new Map(Object.entries(value)), '') +
+        '} ';
     }
-    isfirstElement = false
+    isfirstElement = false;
   });
-  return jsonString
+  return jsonString;
 }
