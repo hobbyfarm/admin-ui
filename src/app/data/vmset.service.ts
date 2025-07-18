5434067c2302ed364c21054b230e11739b48bb61
Tanja Ulmen <149720417+tanemlu@users.noreply.github.com>
2025-07-18 16:41:18 +0200

    feat(): enabled deletion of vms via admin-ui (#243)
    Co-authored-by: Tanja Ulmen <tanja.ulmen@sva.de>

--- src/app/data/vmset.service.ts
+++ src/app/data/vmset.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
 import { switchMap } from 'rxjs/operators';
 import { ServerResponse } from './serverresponse';
 import { of } from 'rxjs';
